d48948b4a7765fb1224d64e7f76a9c23bd0e82c0
akramsameer <akram.sameer99@gmail.com>
2017-07-20 19:06:02 +0200

    CF 236A - Boy or Girl

--- CF 236A - Boy or Girl.cpp
+++ CF 236A - Boy or Girl.cpp
@@ -1,37 +1,38 @@
-#include<cstdio>
-#include<stack>
-
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long int
 using namespace std;
-
-int N, c;
-stack<int> cars;
-
-void marshal() {
-    for(;;) {
-        while(cars.size() > 0) cars.pop();
-        int j = 0;
-        for(int i = 0; i < N; i++) {
-            scanf("%d", &c);
-            if(c == 0) return;
-
-            while(j < N && j != c) {
-                if(cars.size() > 0 && cars.top() == c) break;
-                j++;
-                cars.push(j);
-            }
-            if(cars.top() == c) cars.pop();
-        }
-        if(cars.size() == 0) printf("Yes\n");
-        else printf("No\n");
-    }
+#define clr(v , d)	memset(v , d , sizeof v)
+#define sz(v)		((int)(v).size())
+const int VISITED = 1;
+const int UNVISITED = -1;
+const long long OO = 1e12;
+const int OOI = 1e9;
+#define f first
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+int xdir[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+int ydir[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+void file() {
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	//freopen("out.txt", "w", stdout);
+#else
+	// online submission
+	//freopen("interesting.in", "r", stdin);
+#endif
 }
-
+set<char> s;
+string str;
 int main() {
-    for(;;) {
-        scanf("%d", &N);
-        if(N == 0) break;
+	cin >> str;
+	for (int i = 0; i < sz(str); i++)
+		s.insert(str[i]);
 
-        marshal();
-        printf("\n");
-    }
+	if (sz(s) & 1)
+		printf("IGNORE HIM!");
+	else
+		printf("CHAT WITH HER!");
+
 }
